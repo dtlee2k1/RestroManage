2f96d363ac3780a03a5fe76d9af17d48b86001fb
Le Duc Thai <leducthai2001@gmail.com>
2025-07-06 17:58:51 +0700

    handle order dishes & create UI order list for client

--- src/app/guest/menu/menu-order.tsx
+++ src/app/guest/menu/menu-order.tsx
@@ -2,15 +2,21 @@
 
 import Quantity from '@/app/guest/menu/quantity'
 import { Button } from '@/components/ui/button'
-import { formatCurrency } from '@/lib/utils'
+import { DishStatus } from '@/constants/type'
+import { cn, formatCurrency, handleErrorApi } from '@/lib/utils'
 import { useGetDishListQuery } from '@/queries/useDish'
+import { useGuestOrderMutation } from '@/queries/useGuest'
 import { GuestCreateOrdersBodyType } from '@/schemaValidations/guest.schema'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 import { useMemo, useState } from 'react'
 
 export default function MenuOrder() {
   const { data } = useGetDishListQuery()
   const dishes = useMemo(() => data?.payload.data || [], [data])
+
+  const guestOrderMutation = useGuestOrderMutation()
+  const router = useRouter()
 
   const [orders, setOrders] = useState<GuestCreateOrdersBodyType>([])
 
@@ -42,36 +48,63 @@ export default function MenuOrder() {
     })
   }
 
+  const handleOrder = async () => {
+    if (guestOrderMutation.isPending) return
+    try {
+      await guestOrderMutation.mutateAsync(orders)
+      setOrders([])
+      router.push('/guest/orders')
+    } catch (error) {
+      handleErrorApi({
+        error
+      })
+    }
+  }
+
   return (
     <>
-      {dishes.map((dish) => (
-        <div key={dish.id} className='flex gap-4'>
-          <div className='flex-shrink-0'>
-            <Image
-              src={dish.image}
-              alt={dish.name}
-              height={100}
-              width={100}
-              quality={100}
-              className='object-cover w-[80px] h-[80px] rounded-md'
-            />
+      {dishes
+        .filter((dish) => dish.status !== DishStatus.Hidden)
+        .map((dish) => (
+          <div
+            key={dish.id}
+            className={cn('flex gap-4', dish.status === DishStatus.Unavailable && 'pointer-events-none')}
+          >
+            <div className='flex-shrink-0 relative'>
+              {dish.status === DishStatus.Unavailable && (
+                <span className='absolute inset-0 items-center justify-center flex text-white text-sm text-shadow-lg'>
+                  Hết hàng
+                </span>
+              )}
+              <Image
+                src={dish.image}
+                alt={dish.name}
+                height={100}
+                width={100}
+                quality={100}
+                className='object-cover w-[80px] h-[80px] rounded-md'
+              />
+            </div>
+            <div className='space-y-1'>
+              <h3 className='text-sm'>{dish.name}</h3>
+              <p className='text-xs'>{dish.description}</p>
+              <p className='text-xs font-semibold'>{formatCurrency(dish.price)}</p>
+            </div>
+            <div className='flex-shrink-0 ml-auto flex justify-center items-center'>
+              <Quantity
+                onChange={(value) => handleQuantityChange(dish.id, value)}
+                value={orders.find((order) => order.dishId === dish.id)?.quantity || 0}
+              />
+            </div>
           </div>
-          <div className='space-y-1'>
-            <h3 className='text-sm'>{dish.name}</h3>
-            <p className='text-xs'>{dish.description}</p>
-            <p className='text-xs font-semibold'>{formatCurrency(dish.price)}</p>
-          </div>
-          <div className='flex-shrink-0 ml-auto flex justify-center items-center'>
-            <Quantity
-              onChange={(value) => handleQuantityChange(dish.id, value)}
-              value={orders.find((order) => order.dishId === dish.id)?.quantity || 0}
-            />
-          </div>
-        </div>
-      ))}
+        ))}
       <div className='sticky bottom-0'>
-        <Button className='w-full justify-between'>
-          <span>Giỏ hàng · {totalOrders} món</span>
+        <Button
+          className='w-full justify-between'
+          disabled={orders.length === 0 || guestOrderMutation.isPending}
+          onClick={handleOrder}
+        >
+          <span>Đặt hàng · {totalOrders} món</span>
           <span>{formatCurrency(totalPrice)}</span>
         </Button>
       </div>
