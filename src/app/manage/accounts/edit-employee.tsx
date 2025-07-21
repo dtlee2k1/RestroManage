c19fe7776284faebeba8c91a5fe80452c5f7ddb7
Le Duc Thai <leducthai2001@gmail.com>
2025-07-21 17:00:11 +0700

    handle update role for employee

--- src/app/manage/accounts/edit-employee.tsx
+++ src/app/manage/accounts/edit-employee.tsx
@@ -15,13 +15,15 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Upload } from 'lucide-react'
 import { useEffect, useMemo, useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
-import { Form, FormField, FormItem, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Switch } from '@/components/ui/switch'
 import { useGetAccountQuery, useUpdateAccountMutation } from '@/queries/useAccount'
 import { useUploadMediaMutation } from '@/queries/useMedia'
 import { toast } from 'sonner'
 import { handleErrorApi } from '@/lib/utils'
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
+import { Role, RoleValues } from '@/constants/type'
 
 export default function EditEmployee({
   id,
@@ -48,7 +50,7 @@ export default function EditEmployee({
       password: undefined,
       confirmPassword: undefined,
       changePassword: false,
-      role: 'Employee'
+      role: Role.Employee
     }
   })
   const avatar = form.watch('avatar')
@@ -100,7 +102,7 @@ export default function EditEmployee({
 
   useEffect(() => {
     if (data) {
-      const { name, email, avatar } = data.payload.data
+      const { name, email, avatar, role } = data.payload.data
 
       form.reset({
         name,
@@ -108,7 +110,8 @@ export default function EditEmployee({
         avatar: avatar || undefined,
         changePassword: false,
         password: form.getValues('password'),
-        confirmPassword: form.getValues('confirmPassword')
+        confirmPassword: form.getValues('confirmPassword'),
+        role
       })
     }
   }, [data, form])
@@ -195,6 +198,37 @@ export default function EditEmployee({
                       <Label htmlFor='email'>Email</Label>
                       <div className='col-span-3 w-full space-y-2'>
                         <Input id='email' className='w-full' {...field} />
+                        <FormMessage />
+                      </div>
+                    </div>
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name='role'
+                render={({ field }) => (
+                  <FormItem>
+                    <div className='grid grid-cols-4 items-center justify-items-start gap-4'>
+                      <Label htmlFor='role'>Role</Label>
+                      <div className='col-span-3 w-full space-y-2'>
+                        <Select onValueChange={field.onChange} defaultValue={field.value} value={field.value}>
+                          <FormControl>
+                            <SelectTrigger>
+                              <SelectValue placeholder='Chọn role' />
+                            </SelectTrigger>
+                          </FormControl>
+                          <SelectContent>
+                            {RoleValues.map((role) => {
+                              if (role === Role.Guest) return
+                              return (
+                                <SelectItem key={role} value={role}>
+                                  {role}
+                                </SelectItem>
+                              )
+                            })}
+                          </SelectContent>
+                        </Select>
                         <FormMessage />
                       </div>
                     </div>
